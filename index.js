6027244234d19dbf1fbcb4c032da8197222de9b7
ropenfold <stuartroper@gmail.com>
2019-02-15 08:43:02 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function getQuote() {
+  console.log("get quote");
+  fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      const titleBox = document.getElementById("randomQuote");
+      titleBox.innerHTML = `${data[0].quote}`;
+      document.getElementById("simpsonImage").src = `${data[0].image}`;
+      document.getElementById("character").innerHTML = `${data[0].character}`;
+    });
+}
